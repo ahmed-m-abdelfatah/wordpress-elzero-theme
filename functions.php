f5e1d7e12cfb15fa480bfafd632e9689383bff9c
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-12-24 11:34:55 +0200

    init

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * function to add my custom styles
+ * by: ahmed abdelfatah
+ * wp_enqueue_style($handle, $src, $deps, $ver, $media)
+ * get_template_directory_uri()
+ */
+
+
+
+function add_styles()
+{
+}
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header() ?>
+This is a test content
+<?php get_footer() ?>
